f7bf3d89677d877c2e6def468bc14e4d92165d44
Cioplean Alexandru <alexciop.ac@gmail.com>
2023-05-14 17:49:02 +0300

    database init

--- server/database-seed.sql
+++ server/database-seed.sql
@@ -2,4 +2,4 @@ CREATE TABLE users(username VARCHAR(30) PRIMARY KEY, password VARCHAR(30), city_
 user_id VARCHAR(128), role VARCHAR(256) );
 CREATE TABLE apps(name VARCHAR(30) PRIMARY KEY , description VARCHAR(100), is_running TEXT );
 
-INSERT INTO users (username, password, city_address,want_notify,applications,role) VALUES ("admin", "admin", "Washington Street", "false",  array[]::text[], "admin")
+INSERT INTO users (username, password, city_address,want_notify,applications,role) VALUES ('admin', 'admin', 'Washington Street', 'false',  array[]::text[], 'admin')
