e8b9d9efcc6653e07b114781d32a8525e7d7b2f3
Cioplean Alexandru <alexciop.ac@gmail.com>
2023-06-21 11:56:03 +0300

    reduce number of requests

--- client/src/components/AppItem.js
+++ client/src/components/AppItem.js
@@ -1,85 +1,84 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function AppItem(app){
-    const [appName, setAppName] = useState(app.app.name)
-    const [appDescription, setAppDescr] = useState(app.app.description)
-    const [appRunningState, setAppRunning] = useState(app.app.is_running)
-    const history = useNavigate()
-    
-    const editApp = () => {
-        console.log(appName)
-        localStorage.setItem("appInfo", JSON.stringify({"app_name":appName}));
-        history('/editapp')
-    }
+function AppItem(app) {
+  const [appName, setAppName] = useState(app.app.name)
+  const [appDescription, setAppDescr] = useState(app.app.description)
+  const [appRunningState, setAppRunning] = useState(app.app.is_running)
+  const history = useNavigate()
 
-    return (
-        <div className="app-item">
-            <span>App name
-                <input
-                    className='input-style app-input'
-                    type='text'
-                    value={appName}
-                    onChange={(e) => setAppName(e.target.value)}
-                    required
-                    disabled
-                    />
-            </span>
-           
+  const editApp = () => {
+    console.log(appName)
+    localStorage.setItem("appInfo", JSON.stringify({ "app_name": appName }));
+    history('/editapp')
+  }
 
-            <span>Description
-                <input
-                    className='input-style app-description'
-                    type='textarea'
-                    value={appDescription}
-                    onChange={(e) => setAppDescr(e.target.value)}
-                    required
-                    disabled
-                    />
-            </span> 
+  return (
+    <div className="app-item">
+      <span>App name
+        <input
+          className='input-style app-input'
+          type='text'
+          value={appName}
+          onChange={(e) => setAppName(e.target.value)}
+          required
+          disabled
+        />
+      </span>
 
-            <span>IsRunning
-                {appRunningState == "true" ? (
-                     <div class="led-box">
-                     <div class="led-green"></div>
-                     <p>Running</p>
-                        </div>
-                ) : (
-                    <div class="led-box">
-                    <div class="led-red"></div>
-                    <p>Not Running</p>
-                  </div>
-                )}
-                <input
-                    className='input-style app-input'
-                    type='text'
-                    value={appRunningState}
-                    onChange={(e) => setAppRunning(e.target.value)}
-                    required
-                    disabled
-                    style={{visibility: "hidden"}}
 
-                    />
-            </span>
+      <span>Description
+        <input
+          className='input-style app-description'
+          type='textarea'
+          value={appDescription}
+          onChange={(e) => setAppDescr(e.target.value)}
+          required
+          disabled
+        />
+      </span>
 
-            <span>
-                <button className='edit-delete-button btn'
-                onClick={editApp}
-                  >Edit App
-                    <span className='far fa-solid fa-pen-to-square'>
+      <span>IsRunning
+        {appRunningState == "true" ? (
+          <div class="led-box">
+            <div class="led-green"></div>
+            <p>Running</p>
+          </div>
+        ) : (
+          <div class="led-box">
+            <div class="led-red"></div>
+            <p>Not Running</p>
+          </div>
+        )}
+        <input
+          className='input-style app-input'
+          type='text'
+          value={appRunningState}
+          onChange={(e) => setAppRunning(e.target.value)}
+          required
+          disabled
+          style={{ visibility: "hidden" }}
 
-                    </span>
-                </button>
+        />
+      </span>
 
-                <button className='edit-delete-button btn'
-                    >Delete App
-                    <span className='far fa-solid fa-trash-can'></span>
-                </button>
-            </span>
+      <span>
+        <button className='edit-delete-button btn'
+          onClick={editApp}
+        >Edit App
+          <span className='far fa-solid fa-pen-to-square'>
 
-        </div>
-    );
+          </span>
+        </button>
+
+        <button className='edit-delete-button btn'
+        >Delete App
+          <span className='far fa-solid fa-trash-can'></span>
+        </button>
+      </span>
+
+    </div>
+  );
 };
-
 
 export default AppItem;
--- client/src/components/MyApps.js
+++ client/src/components/MyApps.js
@@ -3,175 +3,135 @@ import axios from 'axios';
 import AppItem from './AppItem';
 
 function MyApps() {
-    const [apps, setApps] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [searchInput, setSearchInput] = useState("");
-    const [typeInput, setTypeInput] = useState("")
+  const [apps, setApps] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+  const [typeInput, setTypeInput] = useState('');
 
-    console.log(searchInput)
-    console.log(typeInput)
+  const handleUploadArchive = () => {
+    const selectedFile = document.getElementById('input').files[0];
+    if (selectedFile) {
+      const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+      const username = userInfo?.username;
 
-    let handleUploadArchive = () => {
-        const selectedFile = document.getElementById("input").files[0];
-        if (selectedFile !== undefined) {
-            const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-            const username = userInfo?.username;
-    
-            const config_app = {
-                headers: {
-                    "Content-type": "multipart/form-data",
-                    "Authorization": userInfo?.role,
-                    "USER-UUID": userInfo?.user_id,
-                },
-                params: {
-                    "username": username,
-                }
-            }
-            const formData = new FormData();
-            formData.append('file',selectedFile)
-    
-            try {
-                axios.post("http://localhost:8080/register/app", formData, config_app)
-            } catch(error){
-                console.log(error)
-                setErrorMessage(error)
-            }
-    
-        }
-       
+      const config_app = {
+        headers: {
+          'Content-type': 'multipart/form-data',
+          Authorization: userInfo?.role,
+          'USER-UUID': userInfo?.user_id,
+        },
+        params: {
+          username: username,
+        },
+      };
+      const formData = new FormData();
+      formData.append('file', selectedFile);
 
-
+      try {
+        axios.post('http://localhost:8080/register/app', formData, config_app);
+      } catch (error) {
+        console.log(error);
+        setErrorMessage(error);
+      }
     }
+  };
 
-    const fetchApps = async () => {
+  const fetchApps = async () => {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    const username = userInfo?.username;
 
-        console.log(typeInput)
+    const config = {
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: userInfo?.role,
+        'USER-UUID': userInfo?.user_id,
+      },
+    };
 
-        //debug filtru nu merge pe frontend
-        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-        const config = {
-            headers: {
-                "Content-type": "application/json",
-                "Authorization": userInfo?.role,
-                "USER-UUID": userInfo?.user_id,
+    try {
+      const response = await axios.get(`http://localhost:8080/user/${username}`, config);
+      const my_apps = response.data?.applications;
+      const query_my_apps = my_apps.join();
+
+      let config_app = {
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: userInfo?.role,
+          'USER-UUID': userInfo?.user_id,
+        },
+        params: {
+          appnames: query_my_apps,
+          username: username,
+        },
+      };
+
+      if (searchInput.length !== 0) {
+        if (typeInput.length === 0 || typeInput === 'name') {
+          config_app = {
+            ...config_app,
+            params: {
+              ...config_app.params,
+              appnames: searchInput,
             },
-        };
-        const username = userInfo?.username;
-        try {
-            const response = await axios.get(`http://localhost:8080/user/${username}`, config);
-            const my_apps = response.data?.applications
-            const query_my_apps = my_apps.join()
-            if (searchInput.length !== 0) {
-                if (typeInput.length === 0 || typeInput === "name") {
-                    const config_app = {
-                        headers: {
-                            "Content-type": "application/json",
-                            "Authorization": userInfo?.role,
-                            "USER-UUID": userInfo?.user_id,
-                        },
-                        params: {
-                            "appnames": searchInput,
-                            "username": username,
-                        }
-                    }
-                    const response_apps = await axios.get(`http://localhost:8080/app`, config_app);
-                    console.log(response_apps)
-                    setApps(response_apps.data.Response)
-                } else {
-                    const config_app = {
-                        headers: {
-                            "Content-type": "application/json",
-                            "Authorization": userInfo?.role,
-                            "USER-UUID": userInfo?.user_id,
-                        },
-                        params: {
-                            "appnames": query_my_apps,
-                            "username": username,
-                            "filter": typeInput + ':' + searchInput,
-                        }
-                    }
-                    const response_apps = await axios.get(`http://localhost:8080/app`, config_app);
-                    console.log(response_apps)
-                   setApps(response_apps.data.Response)
-                }
-               
-            } else {
-                const config_app = {
-                    headers: {
-                        "Content-type": "application/json",
-                        "Authorization": userInfo?.role,
-                        "USER-UUID": userInfo?.user_id,
-                    },
-                    params: {
-                        "appnames": query_my_apps,
-                        "username": username
-                    }
-                }
-
-                const response_apps = await axios.get(`http://localhost:8080/app`, config_app);
-                console.log(response_apps)
-                setApps(response_apps.data.Response)
-            }
-
-
-
-        } catch (error) {
-            console.log(error)
-            setErrorMessage(error)
-            console.log(errorMessage)
-            //to fix wrong search error
-            setApps([]) 
-        };
-    };
-
-    useEffect(() => {
-        fetchApps();
-    }, apps);
-
-
-
-    const renderApps = () => {
-        console.log(apps)
-        if (apps) {
-            return apps.map((app, i) => {
-                return <AppItem key={i} app={app} />
-            })
+          };
+        } else {
+          config_app = {
+            ...config_app,
+            params: {
+              ...config_app.params,
+              filter: typeInput + ':' + searchInput,
+            },
+          };
         }
+      }
 
-    };
+      const response_apps = await axios.get('http://localhost:8080/app', config_app);
+      setApps(response_apps.data.Response);
+    } catch (error) {
+      console.log(error);
+      setErrorMessage(error);
+      setApps([]);
+    }
+  };
 
-    return (
-        <>
-             <input type="submit" value="Submit" onClick={fetchApps}/>
-            <input
-                type="search"
-                placeholder="Search here"
-                onChange={(e) => setSearchInput(e.target.value)}
-                value={searchInput} />
-            <label>
-                Pick filter:
-                <select
-                    value={typeInput}
-                    onChange={(e) => setTypeInput(e.target.value)}>
-                    <option value="name">name</option>
-                    <option value="description">Description(keyword)</option>
-                    <option value="is_running">IsRunning</option>
-                </select>
-            </label>
-            <div className='form-style'>
-                <div className='list-items'>
-                    {renderApps()}
-                </div>
-                <input type="file" id="input" multiple />
-                <input type="submit" value="SubmitArchive" onClick={handleUploadArchive} />
-            
-            </div>
-        
-        </>
-        
-    )
+  useEffect(() => {
+    fetchApps();
+  }, [searchInput, typeInput]);
 
+  const renderApps = () => {
+    if (apps) {
+      return apps.map((app, i) => <AppItem key={i} app={app} />);
+    }
+  };
+
+  return (
+    <>
+      <button type="button" onClick={fetchApps}>
+        Submit
+      </button>
+      <input
+        type="search"
+        placeholder="Search here"
+        onChange={(e) => setSearchInput(e.target.value)}
+        value={searchInput}
+      />
+      <label>
+        Pick filter:
+        <select value={typeInput} onChange={(e) => setTypeInput(e.target.value)}>
+          <option value="name">name</option>
+          <option value="description">Description(keyword)</option>
+          <option value="is_running">IsRunning</option>
+        </select>
+      </label>
+      <div className="form-style">
+        <div className="list-items">{renderApps()}</div>
+        <input type="file" id="input" multiple />
+        <button type="button" onClick={handleUploadArchive}>
+          SubmitArchive
+        </button>
+      </div>
+    </>
+  );
 }
-
 
 export default MyApps;
