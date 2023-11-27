94ac6b7ef128dba1e8f3f2f4c782a7d00855b592
Cioplean Alexandru <alexciop.ac@gmail.com>
2023-11-27 21:18:36 +0200

    refactor home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,52 +5,54 @@ import certs from '../Certs/certs';
 import './Login.scss';
 
 function Home() {
-
-  const [nrMainAppsOwner, setNrMainAppsOwner] = useState(0);
-  const [nrRunningAppsOwner, setRunningAppsOwner] = useState(0);
-  const [nrTotalMainApps, setNrTotalMainApps] = useState(0);
-  const [nrTotalRunningApps, setNrTotalRunningApps] = useState(0);
+  const [userDetails, setUserDetails] = useState({
+    nrMainAppsOwner: 0,
+    nrRunningAppsOwner: 0,
+    nrTotalMainApps: 0,
+    nrTotalRunningApps: 0,
+  });
   const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const fetchDetails = async () => {
       try {
-
         const agent = new Agent({
           cert: certs.certFile,
           key: certs.keyFile,
-        })
+        });
         const userInfo = JSON.parse(localStorage.getItem('userInfo'));
         const username = userInfo?.username;
 
         if (username) {
-          //trigger last time online update
           const response = await axios.get(
-            "https://localhost:443/app/aggregates",
+            'https://localhost:443/app/aggregates',
             {
               headers: {
-                "Content-type": "application/json",
+                'Content-type': 'application/json',
               },
               params: {
-                "username": username,
+                username,
               },
             },
             { httpsAgent: agent },
           );
 
-          console.log(response)
-          const mainAppsOwnerCount = response.data.QueryInfo?.main_apps_owner_count
-          const runningAppsOwnerCount = response.data.QueryInfo?.running_apps_owner_count
-          const mainAppsTotalCount = response.data.QueryInfo?.main_apps_total_count;
-          const runningAppsTotalCount = response.data.QueryInfo?.running_apps_total_count;
-          setNrMainAppsOwner(mainAppsOwnerCount);
-          setRunningAppsOwner(runningAppsOwnerCount);
-          setNrTotalMainApps(mainAppsTotalCount);
-          setNrTotalRunningApps(runningAppsTotalCount);
+          const {
+            main_apps_owner_count,
+            running_apps_owner_count,
+            main_apps_total_count,
+            running_apps_total_count,
+          } = response.data.QueryInfo;
 
+          setUserDetails({
+            nrMainAppsOwner: main_apps_owner_count,
+            nrRunningAppsOwner: running_apps_owner_count,
+            nrTotalMainApps: main_apps_total_count,
+            nrTotalRunningApps: running_apps_total_count,
+          });
         }
       } catch (error) {
-        setErrorMessage("Could not retrieve user details. /" + error.response.data.message);
+        setErrorMessage(`Could not retrieve user details. /${error.response?.data?.message}`);
       }
     };
 
@@ -60,16 +62,15 @@ function Home() {
   return (
     <div className="home-container">
       <h1>Welcome to Cloud Admin!</h1>
-      <p>This platform helps you upload and deploy applications in cloud</p>
-      <nav>
-      </nav>
+      <p>This platform helps you upload and deploy applications in the cloud</p>
+      <nav>{}</nav>
 
       <div className="user-details">
         <ul className="user-info">
-          <li>Number of applications owned : {nrMainAppsOwner} </li>
-          <li>Number of running applications owned : {nrRunningAppsOwner} </li>
-          <li>Total number of applications : {nrTotalMainApps} </li>
-          <li>Total number of running applications : {nrTotalRunningApps} </li>
+          <li>Number of applications owned: {userDetails.nrMainAppsOwner} </li>
+          <li>Number of running applications owned: {userDetails.nrRunningAppsOwner} </li>
+          <li>Total number of applications: {userDetails.nrTotalMainApps} </li>
+          <li>Total number of running applications: {userDetails.nrTotalRunningApps} </li>
         </ul>
       </div>
     </div>
